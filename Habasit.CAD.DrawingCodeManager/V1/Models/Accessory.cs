aca47f09476396a8e8c8dea54c1db7414d7d61da
Thomas.Smith <Thomas.Smith@USSUW-NB0405>
2024-10-08 05:56:07 -0400

    Add project files.

---
+++ Habasit.CAD.DrawingCodeManager/V1/Models/Accessory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Habasit.CAD.DrawingCodeManager.V1.Models;
+public class Accessory : Category
+{
+
+    // Static list of accessories and their codes
+    public static new List<Accessory> Options = new List<Accessory>()
+    {
+        new Accessory("Spiral Belt", "SB"),
+        // Add more based on your data
+    };
+
+    public Accessory(string name, string code)
+    {
+        Name = name;
+        Code = code;
+    }
+}
