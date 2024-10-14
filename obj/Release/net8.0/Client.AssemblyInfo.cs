23bb1ab8f31361954a65fbcae0929f1a57d39241
Thomas Smith <thomas.smith@habasit.com>
2024-10-14 03:43:51 -0400

    Changed vercel.json file. Still trying to fix routing issue.

--- obj/Release/net8.0/Client.AssemblyInfo.cs
+++ obj/Release/net8.0/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+397c3738b0137cea3a2d6eefc8d3fedae444dd6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab2352c1cb5eecf7cbd29f281886fa6b72513481")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
