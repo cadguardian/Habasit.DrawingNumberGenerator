1e56f2a6abdb4085ba0092636ecd0b1117d8f6df
Thomas Smith <thomas.smith@habasit.com>
2024-10-14 01:42:37 -0400

    Added a vercel.json file to hopefully fix routing errors caused by deployment to vercel.

--- obj/Release/net8.0/Client.AssemblyInfo.cs
+++ obj/Release/net8.0/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea116831d89c0ad214035eac2fc6c8630117eda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b713f66014572cf363d38c1c98a33388bad5e44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
