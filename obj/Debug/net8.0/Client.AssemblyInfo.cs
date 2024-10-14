0ea116831d89c0ad214035eac2fc6c8630117eda
Thomas Smith <thomas.smith@habasit.com>
2024-10-14 01:23:20 -0400

    resized form and submission note.

--- obj/Debug/net8.0/Client.AssemblyInfo.cs
+++ obj/Debug/net8.0/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fe4cc033788ab64fee5533c5d8b56687c8a669d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d25271cc10247158569819046c002d527156f6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
