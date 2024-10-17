a811c403cbe37b02fa67ebfc2e5e94bb9ceaeb82
Thomas Smith <Thomas.Smith@habasit.com>
2024-10-17 09:44:03 -0400

    fixed nav and made max width 600 px.

--- obj/Release/net8.0/Client.AssemblyInfo.cs
+++ obj/Release/net8.0/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cede635c28d84a6a291a909dcbc0fdf7f658ee1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a2e81ffd3cd25a4cd48c76ccaa844619209390")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
