60e01323c3dc6e764f3fbcbfa3d0bc176eaf839c
ADDS\Thomas.Smith <tdsmith.cloud@gmail.com>
2024-10-22 15:44:54 -0400

    Published updates

--- DNG.Library.Tests/obj/Release/net8.0/DNG.Tests.AssemblyInfo.cs
+++ DNG.Library.Tests/obj/Release/net8.0/DNG.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DNG.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc2ae27727522e8c0ff9614b819082a79f62043c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be81f940f416fb1250a38cc02323398c5490907d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DNG.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DNG.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
