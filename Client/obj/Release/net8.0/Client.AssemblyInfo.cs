aa5db8e1ff86cf5c3851788b0cbcd8cb118de67b
ADDS\Thomas.Smith <tdsmith.cloud@gmail.com>
2024-11-13 15:35:10 -0500

    Added font awesome icons to each section.

--- Client/obj/Release/net8.0/Client.AssemblyInfo.cs
+++ Client/obj/Release/net8.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("CAD Guardian")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aec5414c9837fc74dc58f8db23da8c7189a96b98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5adcccaa0115c7da6600abaafb0b70a8e426fd09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
